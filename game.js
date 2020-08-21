f895a4b507e2381e623eaaf47950db73d27ca398
SophiaCulver <69135975+SophiaCulver@users.noreply.github.com>
2020-08-20 20:15:40 -0400

    BACKGROUND AND SCORE MOVE STUFF

--- game.js
+++ game.js
@@ -1,170 +1,117 @@
-
 //DEFINE CANVAS AND CONTEXT
-
+//-------------------------------------------------------------------------
 var cvs = document.getElementById('gamecvs');
 //sets a variable which calls the game canvas/container
 var ctx = cvs.getContext('2d');
 //allows the 2d image variant to be drawn on the canvas
-
-
+//--------------------------------------------------------------------------
 
 
 //FRAMERATE EDITING VARIABLES
+//--------------------------------------------------------------------------
 lastFrameTimeMs = 0,
-maxFPS = 100,
-delta = 0,
-timestep = 1000 / maxFPS,
-framesThisSecond = 0,
-lastFpsUpdate = 0;
-score=0;
+    maxFPS = 100,
+    delta = 0,
+    timestep = 1000 / maxFPS,
+    framesThisSecond = 0,
+    lastFpsUpdate = 0;
+//--------------------------------------------------------------------------
 
 
-
-//DEFINE GLOBAL VARIABLES AND CONSTANTS
-const grav = 0.003;      //sets gravitational acceleration
-const jump = 0.8;       //sets instantaneous flap speed setting 
+//DEFINE GLOBAL VARIABLES
+//--------------------------------------------------------------------------
+const grav = 0.0025;      //sets gravitational acceleration
+const jump = 0.75;       //sets instantaneous flap speed setting 
 const DEGREE = Math.PI / 180;
 let frames = 0;
 let pace = 0.2;
 const backgrounds = ['assets/back1.png', 'assets/back2.png']
+let myBackground = new Image()
+myBackground.src = backgrounds[0]
 let position = []
-let hb=false;
-let bgno=0;
+let hb = false;
+//--------------------------------------------------------------------------
 
-//TEMP place for bg
-
-
-class Packet {
-    constructor(socket) {
-        this.ws = socket        
-    }
-
-    sendFlap(bird) {
-        this.ws.send(JSON.stringify(
-            {
-                type: "flap",
-                attr: {
-                    usr: bird.username,
-                    y : bird.y,
-                    vy : -jump
-                }
-            }
-        ))
-    }
-
-    sendDeath(bird) {
-        this.ws.send(JSON.stringify(
-            {
-                type : "death",
-                attr : {
-                    usr : bird.username,
-                    points: bird.points
-                }
-            }
-        ))
-    }
-
-    sendNewPlayer(bird) {
-        this.ws.send(JSON.stringify(
-            {
-                type : "new",
-                attr : {
-                    usr : bird.username,
-                    x: bird.points
-                }
-            }
-        ))
-    }
-
-    sendInfo(bird) {
-        this.ws.send(JSON.stringify(
-            {
-                type : "info",
-                attr : {
-                    usr : bird.username,
-                    x : bird.x,
-                    y : bird.y,
-                    vy : bird.speed
-                }
-            }
-        ))
-    }
-}
-
-var packet = new Packet(new WebSocket("ws://localhost:4000"))
-
-
+//GAME STATES
+//--------------------------------------------------------------------------
 const state = {
     current: 0,
     getReady: 0,
     game: 1,
     over: 2
 }
+//--------------------------------------------------------------------------
 
-
-$("#overlay").on("click", function(){if (myBird.y - myBird.radius <= 0) { return;}myBird.flap();})
+//ASYNCH EVENTLISTENERS
+//--------------------------------------------------------------------------
+$("#overlay").on("click", function () {
+    if (myBird.y - myBird.radius <= 0) { return; }
+    myBird.flap();
+    if (state.current === state.getReady) { state.current++ }
+})
 
 var down = false;
 document.addEventListener('keydown', function (event) {
-    if(down) return;
+    if (down) return;
     down = true;
-   if(event.which===32)
-   {
-    if (myBird.y - myBird.radius <= 0) { return; }
-    myBird.flap();
-   }
+    if (event.which === 32) {
+        if (myBird.y - myBird.radius <= 0) { return; }
+        myBird.flap();
+    }
 
 }, false);
 
-document.addEventListener('keyup', function (event) {
+document.addEventListener('keyup', function (e) {
     down = false;
-    if(event.which>=48 && event.which<=57){bgno (bgno=0) ? 'assets/back2.png': 'assets/back.png';}
-    if(event.which===20){hb = (hb===false) ? true:false;}
+    if (e.which >= 48 && event.which <= 57) { myBackground.src = backgrounds[(parseInt(e.key) - 1) % backgrounds.length] }
+    if (e.which === 20) { hb = (hb === false) ? true : false; }
 }, false);
+//--------------------------------------------------------------------------
 
 
-//BACKGROUND OBJECT
-const bg =
+//BACKGROUND
+//--------------------------------------------------------------------------
+var bg =
 {
-    img:new Image(),
-
-    draw: function()
-    {
-        this.img.src=backgrounds[bgno]
-        ctx.drawImage(this.img, 0, 0, cvs.width, cvs.height)
+    draw: function () {
+        ctx.drawImage(myBackground, 0, 0, cvs.width, cvs.height)
     },
-
 }
+//--------------------------------------------------------------------------
+
 
 //DEFINE CLASS OF BIRD
+//--------------------------------------------------------------------------
 class Bird {
     constructor(x, y, height) {     //CONSTRUCTOR defines (and initializes some) properties which every new bird must contain
         this.username = prompt("Please enter your name", "");
-        this.x = x                     //Birds' x position should always be fixed until death.
-        this.y = y                      //parameters
+        this.score = 0
+        this.x = x                     //parameters
+        this.y = y
         this.height = height
-        this.width = height*1.58333
-        this.radius= height*0.56
+        this.width = height * 1.58333     //sets bird width so it's correct ratio to height
+        this.radius = height * 0.56        //sets radius so it's correct ratio to bird size
         this.frame = 0                  //flapping animation frame init at 0
         this.speed = 0                  //velocity which combined with gravity can be used to obtain new y positions
         this.rotation = 0               //starts rotation off at 0 (These will be measured in ^^DEGREES)
-        this.animationArray = []        //initializes array to hold frames(will be different for each bird so it's empty)
+        this.myAnimationArray = []        //initializes array to hold frames(will be different for each bird so it's empty)
         this.sprite = new Image();        //initializes the sprite as a new image so it can be draw and its src reset
     }
 
 
     draw() {
+        document.getElementById("scoreboard").innerHTML = `<b>${this.score}</b>`;
         ctx.save(); //saves position of all elments
         ctx.translate(this.x, this.y);  //translates all elements
         ctx.rotate(this.rotation);  //rotates to the updated rotation angle
         ctx.drawImage(this.sprite, -this.width / 2, -this.height / 2, this.width, this.height);
         //redraws the bird sprite while the canvas is at this angle
-        if(hb===true){
-            ctx.beginPath();
-            ctx.arc(0+(this.height*0.1), 0 / 8, this.radius, 0, 2 * Math.PI);
-            ctx.strokeStyle = "#FF0000";
-            ctx.lineWidth = 2;
-            ctx.stroke();
+        if (hb === true) {  //if hitboxes have been turned on
+            ctx.beginPath();    //begin defining stroke path
+            ctx.arc(0 + (this.height * 0.1), 0, this.radius, 0, 2 * Math.PI); //"0" because already tanslated to bird x.  2pi radians (full circle) 
+            ctx.strokeStyle = "#FF0000"; //makes the stroke red
+            ctx.lineWidth = 2; //makes the outline of the hitbox slightly thicker
+            ctx.stroke();   //makes the circle
         }
         ctx.restore(); //restores all element from b4 save to original values but keeps the skewed bird :)
 
@@ -175,25 +122,25 @@ class Bird {
     }//so the current downward speed won't affect the height of the jump.
     update(delta) {  //all properties are appropriately updated based on their conditions before redrawing can occur    
         this.frame += frames % delta == 0 ? 1 : 0;
-        this.frame = this.frame % animationArray.length;
+        this.frame = this.frame % myAnimationArray.length;
         if (this.y + this.height / 2 >= cvs.height - fg.height) {
             if (this.speed >= 0) { this.speed = 0 }
             this.rotation = 0;
             this.frame = 2;
-            this.y = cvs.height - fg.height - this.height/2;
+            this.y = cvs.height - fg.height - this.height / 2;
             this.y += this.speed * delta
         }
         else {
             this.speed += grav * delta; //velocity incremented by acceleration pixels/interval^2
-            this.sprite.src = animationArray[this.frame]  //finds the current frame to hold
+            this.sprite.src = myAnimationArray[this.frame]  //finds the current frame to hold
 
 
             this.y += this.speed * delta;   //pos is changed by the speed b/c speed holds pos y change per interval
 
-            if (this.speed >= jump*0.7) {
+            if (this.speed >= jump * 0.6) {
                 this.rotation = 90 * DEGREE;    //has fully finished its flap arc and is now in a nosedive
                 this.frame = 1;
-                if (this.speed <= jump*0.7 + 0.7) { this.rotation = 0 }
+                if (this.speed <= jump * 0.6 + 0.5) { this.rotation = 0 }
             }
 
 
@@ -204,25 +151,30 @@ class Bird {
 
             }
         }
+
     }
 }
+//--------------------------------------------------------------------------
 
 
+//OBJECT PLAYER's BIRD
+//--------------------------------------------------------------------------
 var myBird = new Bird(100, 300, 50)
 {
-    animationArray = ['assets/up.svg', 'assets/mid.svg', 'assets/down.svg', 'assets/mid.svg']
+    myAnimationArray = ['assets/up.svg', 'assets/mid.svg', 'assets/down.svg', 'assets/mid.svg']
 }
+//--------------------------------------------------------------------------
 
 
-
-
+//FOREGROUND
+//--------------------------------------------------------------------------
 var fg = {
     img: new Image(),
 
-    height: cvs.height*0.144,
+    height: cvs.height * 0.144,
     x: 0,
-    y: cvs.height*0.855,
-    width: cvs.width*2,
+    y: cvs.height * 0.855,
+    width: cvs.width * 2,
     draw: function () {
         this.img.src = 'assets/front.png',
             ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
@@ -232,9 +184,11 @@ var fg = {
         this.x <= -448 ? this.x = 0 : this.x -= pace * delta
     }
 }
+//--------------------------------------------------------------------------
 
 
-
+//THE PIPE CLASS
+//--------------------------------------------------------------------------
 class Pipes {
     constructor(gap, y) {
         this.height = 720
@@ -256,28 +210,31 @@ class Pipes {
 
 
     update(delta) {  //all properties are appropriately updated based on their conditions before redrawing can occur       
-        this.x -= pace*delta;
+        this.x -= pace * delta;
         // if the pipes go beyond canvas, we delete them from the array
         if (this.x + this.width <= 0) { position.shift() }
 
+        //COLLISION DETECTION
 
-    
-    if(myBird.x+myBird.height*0.1 + myBird.radius > this.x 
-        && myBird.x+myBird.height*0.1 - myBird.radius < this.x + this.width 
-        && myBird.y + myBird.radius > this.y 
-        && myBird.y - myBird.radius < this.y + this.height){
-        // console.log("Collision T")
-    }
-    if(myBird.x+myBird.height*0.1 + myBird.radius > this.x 
-        && myBird.x+myBird.height*0.1 - myBird.radius < this.x + this.width 
-        && myBird.y + myBird.radius > this.y + this.height + this.gap 
-        && myBird.y - myBird.radius <  this.y + this.height + this.gap + this.height){
-        // console.log("Collision B")
-    }
-    if(this.x+this.width===myBird.x-myBird.radius/2){score++;}
+        if (myBird.x + myBird.height * 0.1 + myBird.radius > this.x
+            && myBird.x + myBird.height * 0.1 - myBird.radius < this.x + this.width
+            && myBird.y + myBird.radius > this.y
+            && myBird.y - myBird.radius < this.y + this.height) {
+                state.current=2;
+        }
+        if (myBird.x + myBird.height * 0.1 + myBird.radius > this.x
+            && myBird.x + myBird.height * 0.1 - myBird.radius < this.x + this.width
+            && myBird.y + myBird.radius > this.y + this.height + this.gap
+            && myBird.y - myBird.radius < this.y + this.height + this.gap + this.height) {
+                state.current=2;
+        }
+
+        //PASS DETECTION
+
+        if (this.x + this.width === myBird.x - myBird.radius / 2) { myBird.score++; }
     }
 }
-
+//--------------------------------------------------------------------------
 
 
 
@@ -291,7 +248,7 @@ function drawAll() {
     fg.draw()
 }
 
-function updateAll(delta){
+function updateAll(delta) {
     myBird.update(delta)
     fg.update(delta)
     for (var i = 0; i < position.length; i++) {
@@ -315,37 +272,25 @@ function mainLoop(timestamp) {
     var numUpdateSteps = 0;
     while (delta >= timestep) {
         updateAll(timestep);
-        if (frames % 200*delta === 0) {position.push(new Pipes(Math.random() * 30 + 170, Math.random() * 350 - 690)) }
+        if (frames % 200 * delta === 0) { position.push(new Pipes(Math.random() * 30 + 190, Math.random() * 350 - 690)) }
         frames++;
         delta -= timestep;
         if (++numUpdateSteps >= 240) {
             panic();
             break;
         }
-        
+
     }
     
-    
-    
+
+
+
     drawAll()
     requestAnimationFrame(mainLoop)
 
 
 
-    
+
 }
 
 requestAnimationFrame(mainLoop)
-
-
-    // document.getElementById("overlay").innerHTML = ""
-
-
-
-
-
-    // var loops;
-// startgamestate = setInterval(function(){
-//     loops++
-//     if(loops=5){clearInterval(startgamestate)}
-// },1000)
